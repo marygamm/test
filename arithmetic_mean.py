9af937cd687b3a44ac59a534f7742eddca323a49
Mary Gamova <marygam9963pb@gmail.com>
2024-10-05 15:05:02 +0300

    arithmetic_mean task creation.py

---
+++ arithmetic_mean.py
@@ -0,0 +1,12 @@
+def arithmetic_mean(x): #среднее арифметическое списка x
+    summ = 0
+    if not x:
+        return "list is empty"
+    else:
+        for num in x:
+            summ += int(num)
+        return summ / len(x)
+    
+s = input()
+x = s.split()
+print(arithmetic_mean(x))
